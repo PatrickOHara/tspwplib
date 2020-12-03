dc5e170e5a0e2d3f7af47df3a6166f97700514c2
PatrickOHara <pohara@turing.ac.uk>
2020-12-03 11:25:28 +0000

    🔖 Version and workflow

--- setup.py
+++ setup.py
@@ -1,25 +1,27 @@
 """Setup script for tspwplib"""
 
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     author="Patrick O'Hara",
-    author_email="pohara@turing.ac.uk",
+    author_email="patrick.h.o-hara@warwick.ac.uk",
+    url="https://github.com/PatrickOHara/tspwplib",
     description="Library of instances for TSP with Profits",
     install_requires=["tsplib95"],
     name="tspwplib",
     packages=["tspwplib"],
     python_requires=">=3.6",
+    license="MIT",
     classifiers=[
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3 :: Only',
-        'Operating System :: OS Independent',
-        'License :: MIT License',
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3 :: Only",
+        "Operating System :: OS Independent",
+        "License :: MIT License",
     ],
     use_scm_version=True,
-    setup_requires=['setuptools_scm'],
+    setup_requires=["setuptools_scm"],
 )
